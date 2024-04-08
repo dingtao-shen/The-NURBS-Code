b942d3ac8f4004c30feebb05da4db2ae824c3d2c
dtshen <dtshen@outlook.com>
2024-04-08 10:43:27 +0800

    update README

--- minurbs/test/test.cpp
+++ minurbs/test/test.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <iomanip>
 #include <vector>
 #include <cmath>
 using namespace std;
@@ -28,9 +29,19 @@ int main(){
 
     minurbs::RationalCurve crv(2, K, CP, W);
 
-    VectorXd p = curvePoint(crv, 0.75);
-    cout << p.transpose() << endl;
-    cout << pow(p(0), 2) + pow(p(1), 2) << endl;
+    // cout << "para  x  y  x^2+y^2" << endl;
+    // for(int i = 0; i <= 10; i++){
+    //     VectorXd p = curvePoint(crv, 0.1 * i);
+    //     cout << 0.1*i << " " << p(0) << " " << p(1) << " " << pow(p(0), 2) + pow(p(1), 2) << endl;
+    // }
+    cout << left << setw(10) << "para" << setw(25) << "x" << setw(25) << "y" << setw(25) << "x^2+y^2" << endl;
+    for(int i = 0; i <= 10; i++){
+        VectorXd p = curvePoint(crv, 0.1 * i);
+        cout << left << setw(10) << setprecision(2) << fixed << 0.1*i
+             << setw(25) << setprecision(16) << p(0)
+             << setw(25) << setprecision(16) << p(1)
+             << setw(25) << setprecision(16) << pow(p(0), 2) + pow(p(1), 2) << endl;
+    }
 
 	return 0;
 }
