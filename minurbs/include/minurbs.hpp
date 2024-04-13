24332a5a96fd504eb46f7c73ca10be4ee9ae191e
dtshen <dtshen@outlook.com>
2024-04-13 18:19:13 +0800

    update minurbs.hpp

---
+++ minurbs/include/minurbs.hpp
@@ -0,0 +1,10 @@
+/**
+ * Import the entire library into the tinynurbs namespace.
+ * 
+ * Use of this source code is governed by a BSD-style license that can be found in
+ * the LICENSE file.
+ */
+
+#include "core/basis.hpp"
+#include "core/curve.hpp"
+#include "core/evaluate.hpp"
